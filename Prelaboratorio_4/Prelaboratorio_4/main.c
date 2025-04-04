e762bebd1c8ba4056d9e7e3b62a36e4f95779b8a
Yelen328 <cot231076@uvg.edu.gt>
2025-04-04 13:55:47 -0600

    prelaboratorio

--- Prelaboratorio_4/Prelaboratorio_4/main.c
+++ Prelaboratorio_4/Prelaboratorio_4/main.c
@@ -4,26 +4,59 @@
  * Created: 4/4/2025 
  * Author : yelena cotzojay
  */ 
-#define		F_CPU	16000000
+#define	F_CPU	16000000
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
+
+volatile uint8_t	contador=0; //Contador de núero entero de 8 bits
+volatile uint8_t	estado=0X00;
 
 
+//Función Prototypes
+void setup();
+void INIT_PIN_CHANGE();
+
+//main
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	
+    setup();
+	while (1) {
+		PORTB = contador;  // Actualiza PORTB con el valor del contador
+		
+	}
+	
 }
 
 void setup()
 {
-	DDRB=0xFF;
-	PORTB=0x00;
-	DDRC&=~((1<<PB2)|(1<<PB3));
-	PORTC&=~((1<<PB2)|(1<<PB3));
-	PORTC|=(1<<PB2)|(1<<PB3);	
+	cli();
+	DDRB =0xFF;		//Configurarlos como saldia
+	PORTB =0xff;		//Inicialmente apagado
+	DDRC =0x00;	//Configurar el bit 2 y 3 del puerto C como entrada
+	PORTC|=(1<<PORTC2)|(1<<PORTC3);		//Pull up activado
+	INIT_PIN_CHANGE();
+	sei();
 }
 
-//INTERRUPCIONES 
+void INIT_PIN_CHANGE()
+{//Habilitar interrupciones de pin change 
+	PCICR |= (1 << PCIE1);     // Habilitar interrupción de cambio de estado 
+	PCMSK1 |= (1 << PCINT10);  // Habilita interrupción para PORTC2 (bit 2 de Puerto C)
+	PCMSK1 |= (1 << PCINT11);  // Habilita interrupción para PORTC3 (bit 3 de Puerto C)
+}
+
+//Vector de interupción:
+ISR(PCINT1_vect) {
+	//Leer estado actua de los botones
+	uint8_t	estado_actual=PINC;
+	
+	//Detectar flanco
+	if (~(estado_actual & (1<<PORTC2))){
+		contador++;
+	}
+	else if (~(estado_actual & (1<<PORTC3))){
+		contador--;
+	}
+	} 
