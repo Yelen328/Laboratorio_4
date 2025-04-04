55616d97cdca47d4b706acb5305fe02098d88337
Yelen328 <cot231076@uvg.edu.gt>
2025-04-04 15:37:53 -0600

    modificaciones

---
+++ Lab_4/Lab_4/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab_4.c
+ *
+ * Created: 4/4/2025 15:35:23
+ * Author : yelen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
