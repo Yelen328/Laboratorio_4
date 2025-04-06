89d88908be79216eb5c91982487cc183dea061f9
Yelen328 <cot231076@uvg.edu.gt>
2025-04-06 16:31:16 -0600

    Laboratorio funcinal
    Funciona parcialmente bien, falta corregir el multiplexeo

---
+++ Postlaboratorio_4/Postlaboratorio_4/main.c
@@ -0,0 +1,18 @@
+/*
+ * Postlaboratorio_4.c
+ *
+ * Created: 6/4/2025 16:29:50
+ * Author : yelen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
